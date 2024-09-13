da3d955cf692111a7ac92ee60793ac798b841fe1
javad <javadmohammadi.js@gmail.com>
2024-09-13 08:25:04 -0600

    add exmple 3 about modal layout

--- src/design-patterns/layout/index.jsx
+++ src/design-patterns/layout/index.jsx
@@ -2,6 +2,8 @@ import SplitScreen from "./example-1/split-screen";
 import { RegularList } from "./example-2/lists/Regular";
 import{authors} from "./example-2/data/authors"
 import { SmallAuthorListItem } from "./example-2/authors/SmallListItems";
+import { LargeAuthorListItem } from "./example-2/authors/LargeListItems";
+import { Modal } from "./example-2/modal";
 
 const LeftSideComp = ({ title }) => {
     return <h2 style={{ background: 'lime' }}> {title}</h2>
@@ -24,7 +26,16 @@ export default function Layout() {
             <div className='example'>
             <div>
                 <h3>lists example</h3>
+                <p className='intro'>small list</p>
                 <RegularList items={authors} sourceName='author' ItemComponent={SmallAuthorListItem} />
+                <p className='intro'>large list</p>
+                <RegularList items={authors} sourceName='author' ItemComponent={LargeAuthorListItem} />
+            </div>
+            <div className='example'>
+                <h3>Modal</h3>
+                <Modal>
+                <RegularList items={authors} sourceName='author' ItemComponent={LargeAuthorListItem} />
+                </Modal>
             </div>
             </div>
         </div>
