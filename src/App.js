f2748bd197b7c3e3c2529202f73fd8c9dd2413ec
javad <javadmohammadi.js@gmail.com>
2024-09-16 09:50:12 -0600

    remove imported container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import Container from "./design-patterns/container-presentational-pattern/example-2";
 
 export default function App(){
   return (
     <div>
       <h2>React.js concepts like design patterns, hooks, performance</h2>
-        <Container/>
     </div>
   )
 }
